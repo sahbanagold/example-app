b1debce226660df424c67e2ac0da525f419971cd
imam.nugraha23@gmail.com <imam.nugraha23@gmail.com>
2016-09-23 11:41:08 +0700

    230916

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/admin', function(req, res, next) {
+  res.render('admin', { title: 'Express' });
+});
 
 module.exports = router;
